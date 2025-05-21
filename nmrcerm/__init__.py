82e554aab163aa52247134e42c4d786b3aba99d0
sanlopez <irene.sanchezl93@gmail.com>
2025-05-21 12:12:17 +0200

    print-project action added

--- nmrcerm/__init__.py
+++ nmrcerm/__init__.py
@@ -1,6 +1,6 @@
 import core
-from nmrcerm.constants import ACTION_GENERATE_EXPERIMENT_METADATA
-from nmrcerm.actions import generate_experiment_metadata
+from nmrcerm.constants import ACTION_GENERATE_EXPERIMENT_METADATA, ACTION_PRINT_PROJECT
+from nmrcerm.actions import generate_experiment_metadata, print_project
 
 
 class Plugin(core.Plugin):
@@ -31,3 +31,4 @@ class Plugin(core.Plugin):
     @classmethod
     def define_methods(cls):
         cls.define_method(ACTION_GENERATE_EXPERIMENT_METADATA, generate_experiment_metadata.perform_action)
+        cls.define_method(ACTION_PRINT_PROJECT, print_project.perform_action)
--- nmrcerm/db/sqlite_db.py
+++ nmrcerm/db/sqlite_db.py
@@ -14,3 +14,19 @@ def update_project(project_name, key, value):
     finally:
         if connection:
             close_connection_to_ddbb(connection)
+
+
+def get_project_info(project_name):
+    connection = None
+    try:
+        connection = connect_to_ddbb()
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM project_info WHERE project_name = ?', (project_name,))
+        project_info = cursor.fetchall()
+        column_names = [columns[0] for columns in cursor.description]
+        return column_names, project_info
+    except Exception as e:
+        print(f'... could not check projects because of: {e}')
+    finally:
+        if connection:
+            close_connection_to_ddbb(connection)
